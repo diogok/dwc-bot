e341fccd099e884b915294d403b960521f4d6564
Diogo Silva <diogo@diogok.net>
2015-12-10 19:13:56 -0200

    basic API doc

--- src/dwc_bot/web.clj
+++ src/dwc_bot/web.clj
@@ -22,6 +22,9 @@
   {[:get "/"] 
      (fn [req]
        {:status 302 :headers {"Location" "/index.html"}})
+   [:get "/docs"]
+     (fn [req]
+       {:status 302 :headers {"Location" "/docs/index.html"}})
    [:get "/inputs"]
     (fn [req] (result (map #(.replace % "/rss.do" "") (core/get-inputs))))
    [:post "/inputs"]
